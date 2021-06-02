a185307a9a2c1213e1f694b5a01a1ce36e27c7de
theo <theo.lanique@gmail.com>
2021-06-02 17:57:20 +0200

    Fix response in time cookies, seed & send message

--- server/main.js
+++ server/main.js
@@ -16,31 +16,42 @@ class Server {
         user.socket.on('close', () => this.#leaveRoom(user));
 
         if (!this._rooms[user.ip]) {
-            this._rooms[user.ip] = { [user.id]: user.socket };
+            this._rooms[user.ip] = { [user.id]: user };
         } else {
-            this._rooms[user.ip][user.id] = user.socket;
+            this._rooms[user.ip][user.id] = user;
+        }
+
+        for (let peer in this._rooms[user.ip]) {
+            let socketPeer = this._rooms[user.ip][peer];
+            socketPeer.socket.send(JSON.stringify({ infos: socketPeer.getInfo(), type: 'join' }));
         }
     }
 
     #leaveRoom(user) {
-        if (!this._rooms[user.ip] && !this._rooms[user.ip][user.id]) return;
+        if (!this._rooms[user.ip] || !this._rooms[user.ip][user.id]) return;
 
         delete this._rooms[user.ip][user.id];
 
         if (Object.keys(this._rooms[user.ip]).length === 0) delete this._rooms[user.ip];
+
+        for (let peer in this._rooms[user.ip]) {
+            let socketPeer = this._rooms[user.ip][peer];
+            socketPeer.socket.send(JSON.stringify({ infos: socketPeer.getInfo(), type: 'leave' }));
+        }
     }
 
     #setCookie(headers, response) {
         if (response.headers.cookie && response.headers.cookie.indexOf('userid=') !== -1) return;
         const { v4: uuidv4 } = require('uuid');
         const id = uuidv4();
+        response.userid = id;
         headers.push(`Set-Cookie: userid=${id}; SameSite=Strict; Secure`);
     }
 }
 
 class User {
     constructor(socket, req) {
-        this.id = req.headers.cookie.replace('userid=', '');
+        this.id = req.userid || req.headers.cookie.replace('userid=', '');
         this.socket = socket;
         this.#setIp(req);
     }
@@ -55,7 +66,14 @@ class User {
         if (this.ip == '::1' || this.ip == '::ffff:127.0.0.1') this.ip = '127.0.0.1';
     }
 
-    static getName(seed) {
+    getInfo() {
+        return {
+            name: this.getName(User.getSeed(this.id)),
+            id: this.id,
+        };
+    }
+
+    getName(seed) {
         const { uniqueNamesGenerator, animals, colors } = require('unique-names-generator');
         const displayName = uniqueNamesGenerator({
             length: 2,
@@ -65,6 +83,19 @@ class User {
             seed: seed,
         });
         return displayName;
+    }
+
+    static getSeed(id) {
+        let seed = '';
+        for (let char of id) {
+            if (isNaN(char)) {
+                seed += char.charCodeAt();
+            } else {
+                seed += char;
+            }
+            if (seed.length >= 15) break;
+        }
+        return +seed;
     }
 }
 
