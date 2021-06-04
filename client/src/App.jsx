010f63fef6fdfac0dd39c5782133dd37b34435bf
theo <theo.lanique@gmail.com>
2021-06-04 11:52:52 +0200

    Starting ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <div className="display-info">
+                <div className="instruction">Open Mindrop on other devices to send files or messages</div>
+            </div>
+            <div className="footer">
+                <div className="circle"></div>
+                <div className="aka">
+                    You can be discovered on the network as <div className="name">Salamon Spoonbill</div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
