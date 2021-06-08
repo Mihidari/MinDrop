55bb0100f2ecfa332285a2fda2dab53fc5d2f272
theo <theo.lanique@gmail.com>
2021-06-08 13:22:35 +0200

    Fix peer leave

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,9 @@ import Device from './components/Device';
 
 function App() {
     const [name, setName] = useState('');
-    const [device, setDevice] = useState('');
     const [peers, setPeers] = useState([]);
+
+    const stockPeers = [];
 
     useEffect(() => {
         const ws = new WebSocket('ws://localhost:3387');
@@ -15,7 +16,17 @@ function App() {
             if (data.type === 'join' && data.infos.id === document.cookie.replace('userid=', '')) {
                 setName(data.infos.name);
             } else if (data.type === 'join') {
-                setPeers([...peers, data.infos]);
+                setPeers([...stockPeers, data.infos]);
+                stockPeers.push(data.infos);
+            }
+
+            if (data.type === 'leave') {
+                for (let i = 0; i < stockPeers.length; i++) {
+                    if (data.infos.id === stockPeers[i].id) {
+                        stockPeers.splice(i, 1);
+                        setPeers([...stockPeers]);
+                    }
+                }
             }
         };
 
@@ -35,7 +46,7 @@ function App() {
                     <path d="M11 17h2v-6h-2v6zm1-15C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zM11 9h2V7h-2v2z"></path>
                 </svg>
             </div>
-            {peers.length > -1 ? (
+            {peers.length > 0 ? (
                 <div className="display-send">
                     <div className="instruction-send">Left click to send files, right click to send message</div>
                     <div className="devices">
