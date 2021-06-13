071b687e37909999abab163825df1b9e99acbfad
theo <theo.lanique@gmail.com>
2021-06-13 14:09:03 +0200

    Fix link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -82,11 +82,11 @@ function App() {
     return (
         <div className="App">
             <div className="header">
-                <svg className="info icon">
-                    <a href="https://github.com/Mihidari/MinDrop" rel="noreferrer" target="_blank">
+                <a href="https://github.com/Mihidari/MinDrop" rel="noreferrer" target="_blank">
+                    <svg className="info icon">
                         <path d="M11 17h2v-6h-2v6zm1-15C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zM11 9h2V7h-2v2z"></path>
-                    </a>
-                </svg>
+                    </svg>
+                </a>
             </div>
             {peers.length > 0 ? (
                 <div className="display-send">
